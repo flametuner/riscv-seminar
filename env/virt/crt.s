b7b0f536c474c342fc4e1baba25a985402e9b39e
Gustavo Inácio <incio.gusta@gmail.com>
2020-10-10 15:58:52 -0300

    Simple version

--- env/virt/crt.s
+++ env/virt/crt.s
@@ -1,113 +1,19 @@
-# See LICENSE for license details.
-
-.equ REGBYTES, 8
-
-.macro lxsp a, b
-ld \a, ((\b)*REGBYTES)(sp)
-.endm
-
-.macro sxsp a, b
-sd \a, ((\b)*REGBYTES)(sp)
-.endm
-
 .equ MAX_HARTS,    4
-.equ SAVE_REGS,    16
 .equ STACK_SIZE,   1024
 .equ STACK_SHIFT,  10
-.equ CONTEXT_SIZE, (SAVE_REGS * REGBYTES)
 
-.globl _text_start
-.globl _text_end
-.globl _rodata_start
-.globl _rodata_end
-.globl _data_start
-.globl _data_end
-.globl _bss_start
-.globl _bss_end
-.global _memory_start;
-.global _memory_end;
-
-#
-# start of trap handler
-#
-
-.section .text.init,"ax",@progbits
+.section .text.init,"ax"
 .globl _start
 
 _start:
-    # setup default trap vector
-    la      t0, trap_vector
-    csrw    mtvec, t0
-
     # set up stack pointer based on hartid
     csrr    t0, mhartid
     slli    t0, t0, STACK_SHIFT
     la      sp, stacks + STACK_SIZE
     add     sp, sp, t0
 
-    # park all harts excpet hart 0
-    csrr    a0, mhartid
-    bnez    a0, park
-
     # jump to libfemto_start_main
-    j       libfemto_start_main
+    j       main
 
-    # sleeping harts mtvec calls trap_fn upon receiving IPI
-park:
-    wfi
-    j       park
-
-    .align 2
-trap_vector:
-    # Save registers.
-    addi    sp, sp, -CONTEXT_SIZE
-    sxsp    ra, 0
-    sxsp    a0, 1
-    sxsp    a1, 2
-    sxsp    a2, 3
-    sxsp    a3, 4
-    sxsp    a4, 5
-    sxsp    a5, 6
-    sxsp    a6, 7
-    sxsp    a7, 8
-    sxsp    t0, 9
-    sxsp    t1, 10
-    sxsp    t2, 11
-    sxsp    t3, 12
-    sxsp    t4, 13
-    sxsp    t5, 14
-    sxsp    t6, 15
-
-    # Invoke the handler.
-    mv      a0, sp
-    csrr    a1, mcause
-    csrr    a2, mepc
-    jal     trap_handler
-
-    # Restore registers.
-    lxsp    ra, 0
-    lxsp    a0, 1
-    lxsp    a1, 2
-    lxsp    a2, 3
-    lxsp    a3, 4
-    lxsp    a4, 5
-    lxsp    a5, 6
-    lxsp    a6, 7
-    lxsp    a7, 8
-    lxsp    t0, 9
-    lxsp    t1, 10
-    lxsp    t2, 11
-    lxsp    t3, 12
-    lxsp    t4, 13
-    lxsp    t5, 14
-    lxsp    t6, 15
-    addi sp, sp, CONTEXT_SIZE
-
-    # Return
-    mret
-
-    .bss
-    .align 4
-    .global stacks
 stacks:
     .skip STACK_SIZE * MAX_HARTS
--- examples/hello/hello.c
+++ examples/hello/hello.c
@@ -1,6 +1,34 @@
-#include <stdio.h>
+static volatile char *uart;
 
-int main(int argc, char **argv)
+int init()
 {
-	printf("Hello world\n");
+	uart = (char *)(void *)0x10000000;
+}
+
+static int putchar(int ch)
+{
+    while ((uart[0x05] & 0x40) == 0);
+    return uart[0x00] = ch & 0xff;
+}
+
+
+int print(const char *s)
+{
+    while (*s) putchar(*s++);
+}
+
+void poweroff(int status)
+{
+	volatile int *test = (int *)(void *) 0x100000;
+    *test = 0x5555;
+    while (1) {
+        asm volatile("");
+    }
+}
+
+int main()
+{
+	init();
+	print("Hello World\n");
+	poweroff(0);
 }
