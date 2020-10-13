7147fbea65742353b4206d76872cec8218d97101
Gustavo Inácio <incio.gusta@gmail.com>
2020-10-13 16:42:41 -0300

    Added comments

--- examples/hello/hello.c
+++ examples/hello/hello.c
@@ -1,5 +1,9 @@
 static volatile char *uart;
 
+/**
+ *  Inicializa o UART da maquina `virt` localizada no endereço de memória 0x10000000
+ * 
+ */
 int init()
 {
 	uart = (char *)(void *)0x10000000;
@@ -7,16 +11,24 @@ int init()
 
 static int putchar(int ch)
 {
+    // Checkagem para ver se o UART já leu o caractere anterior
     while (uart[0x00] < 0);
+    // Seta o caractere no registrador do uart
     return uart[0x00] = ch & 0xff;
 }
 
 
+/**
+ * Loop para todas printar array de caracteres
+ */
 int print(const char *s)
 {
     while (*s) putchar(*s++);
 }
 
+/**
+ * Desliga a máquina com status code `status`
+ */
 void poweroff(int status)
 {
 	volatile int *test = (int *)(void *) 0x100000;
